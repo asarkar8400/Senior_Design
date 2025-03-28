0b397a880a7c73f088a4dfb27d8b11fd88cb560b
Aritro Sarkar <74608503+asarkar8400@users.noreply.github.com>
2025-03-28 11:33:01 -0400

    Update sensitivity_control.vhd

--- Sensitivity_Control/sensitivity_control.vhd
+++ Sensitivity_Control/sensitivity_control.vhd
@@ -16,12 +16,12 @@ begin
 	process (all) is
  	begin
     	if (sensitivity_switch = "00") then 
-			divided_peak_power <= peak_power;			   		
+			divided_peak_power <= peak_power;		 --don't shift if ss = 0	   		
 		elsif (sensitivity_switch = "01") then 
-			divided_peak_power <= peak_power srl 2;
+			divided_peak_power <= peak_power srl 1;	 --shift right by 1	if ss = 1
 		elsif (sensitivity_switch = "10") then
-			divided_peak_power <= peak_power srl 4;
-		else divided_peak_power <= peak_power srl 8;
+			divided_peak_power <= peak_power srl 2;	 --shift right by 2	if ss = 2
+		else divided_peak_power <= peak_power srl 3; --shift right by 3 if ss = 3
 		end if;
 	end process;
 end sensitivity_control;
